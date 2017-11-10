496549e790c71e9fd001222bb6b65283fd9cbf78
Grégory Salvan <apieum@gmail.com>
2017-11-10 11:22:14 +0000

    Import describe, it, test from another package, and testsets from Base.Test, tests basics

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,60 @@
 using Maracas
 using Base.Test
+import Maracas.AbstractTestSet
+@testset "Maracas" begin
+    @testset "'describe' returns a TestSet" begin
+        @test isa(describe(()->nothing, "description"), AbstractTestSet)
+    end
 
-# write your own tests here
-@test 1 == 2
+    @testset "'describe' TestSet contains description" begin
+        expected = "expected description"
+        ts = describe(()->nothing, expected)
+        @test contains(ts.description, expected)
+    end
+
+    @testset "'describe' TestSet description is colored in yellow" begin
+        ts = describe(()->nothing, "description")
+        @test contains(ts.description, Base.text_colors[:yellow])
+    end
+
+    @testset "'it' returns a TestSet" begin
+        @test isa(it(()->nothing, "description"), AbstractTestSet)
+    end
+
+    @testset "'it' TestSet contains description" begin
+        expected = "expected description for it"
+        ts = it(()->nothing, expected)
+        @test contains(ts.description, expected)
+    end
+
+    @testset "'it' TestSet contains [Spec]" begin
+        ts = it(()->nothing, "description")
+        @test contains(ts.description, "[Spec]")
+    end
+
+    @testset "'it' TestSet description is colored in cyan" begin
+        ts = it(()->nothing, "description")
+        @test contains(ts.description, Base.text_colors[:cyan])
+    end
+
+    @testset "'test' returns a TestSet" begin
+        @test isa(test(()->nothing, "description"), AbstractTestSet)
+    end
+
+    @testset "'test' TestSet contains description" begin
+        expected = "expected description for it"
+        ts = test(()->nothing, expected)
+        @test contains(ts.description, expected)
+    end
+
+    @testset "'test' TestSet contains [Test]" begin
+        ts = test(()->nothing, "description")
+        @test contains(ts.description, "[Test]")
+    end
+
+    @testset "'test' TestSet description is colored in blue" begin
+        ts = test(()->nothing, "description")
+        @test contains(ts.description, Base.text_colors[:blue])
+
+    end
+end
