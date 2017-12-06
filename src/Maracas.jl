fd4663fb160da6ccef4642cb980baca4ecdee48c
Grégory Salvan <apieum@gmail.com>
2017-12-06 19:12:00 +0100

    test titles are not necerally bold, and fix a typo on env var

--- src/Maracas.jl
+++ src/Maracas.jl
@@ -6,18 +6,21 @@ export describe, test, it, MARACAS_SETTING
 
 using Compat.Test
 const MARACAS_SETTING = Dict(
-    "error" => Symbol(get(ENV, "MARARAS_ERROR", :red)),
-    "warn" =>  Symbol(get(ENV, "MARARAS_WARN", :yellow)),
-    "pass" =>  Symbol(get(ENV, "MARARAS_PASS", :green)),
-    "info" =>  Symbol(get(ENV, "MARARAS_INFO", :blue)),
-    "default" => Base.text_colors[Symbol(get(ENV, "MARARAS_DEFAULT", :normal))],
-    "bold" => Base.text_colors[Symbol(get(ENV, "MARARAS_BOLD", :bold))],
-    "margin" => UInt(get(ENV, "MARARAS_MARGIN", 10)),
-    "title" =>  Base.text_colors[Symbol(get(ENV, "MARARAS_TITLE", :yellow))],
-    "spec" =>  Base.text_colors[Symbol(get(ENV, "MARARAS_SPEC", :cyan))],
-    "test" =>  Base.text_colors[Symbol(get(ENV, "MARARAS_TEST", :blue))],
-
+    "error" => Symbol(get(ENV, "MARACAS_ERROR", :red)),
+    "warn" =>  Symbol(get(ENV, "MARACAS_WARN", :yellow)),
+    "pass" =>  Symbol(get(ENV, "MARACAS_PASS", :green)),
+    "info" =>  Symbol(get(ENV, "MARACAS_INFO", :blue)),
+    "default" => Base.text_colors[Symbol(get(ENV, "MARACAS_DEFAULT", :normal))],
+    "bold" => Base.text_colors[Symbol(get(ENV, "MARACAS_BOLD", :bold))],
+    "margin" => UInt(get(ENV, "MARACAS_MARGIN", 10)),
+    "test" =>  "",
+    "title" =>  "",
+    "spec" =>  "",
 )
+MARACAS_SETTING["test"] =  get(ENV, "MARACAS_TEST", string(Base.text_colors[:blue], MARACAS_SETTING["bold"]))
+MARACAS_SETTING["title"] =  get(ENV, "MARACAS_TITLE", string(Base.text_colors[:yellow], MARACAS_SETTING["bold"]))
+MARACAS_SETTING["spec"] =  get(ENV, "MARACAS_SPEC", string(Base.text_colors[:cyan], MARACAS_SETTING["bold"]))
+
 if VERSION < v"0.6"
     print_with_color(args...;kwargs...) = Base.print_with_color(args...)
     TestSetException(pass::Int64, fail::Int64, error::Int64, broken::Int64, errors_and_fails::Array{Any,1}) = Base.Test.TestSetException(pass, fail, error, broken)
@@ -36,15 +39,15 @@ function maracas(tests, desc)
     finish(ts)
 end
 function describe(tests::Function, desc)
-    desc = string(MARACAS_SETTING["title"], MARACAS_SETTING["bold"], desc, MARACAS_SETTING["default"], )
+    desc = string(MARACAS_SETTING["title"], desc, MARACAS_SETTING["default"], )
     maracas(tests, desc)
 end
 function it(tests::Function, desc)
-    desc = string(MARACAS_SETTING["spec"], MARACAS_SETTING["bold"], "[Spec] ", MARACAS_SETTING["default"], "it ", desc)
+    desc = string(MARACAS_SETTING["spec"], "[Spec] ", MARACAS_SETTING["default"], "it ", desc)
     maracas(tests, desc)
 end
 function test(tests::Function, desc)
-    desc = string(MARACAS_SETTING["test"], MARACAS_SETTING["bold"], "[Test] ", MARACAS_SETTING["default"], desc)
+    desc = string(MARACAS_SETTING["test"], "[Test] ", MARACAS_SETTING["default"], desc)
     maracas(tests, desc)
 end
 
